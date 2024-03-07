e51f81694071a93d66b72db333ef1a05d7124bf5
Solomon Gachure <gachuresolomon64@gmail.com>
2024-03-07 22:57:33 +0300

    Slight changes

--- src/component/drinks.jsx
+++ src/component/drinks.jsx
@@ -29,7 +29,7 @@ console.error('Error fetching data', error)
           <h1 className='text-2xl lg:text-3xl font-bold text-orange-500 flex justify-center'>Quench Your Thirst : </h1>
           <div className='grid md:grid-cols-2 gap-4 text-md pt-4 w-full mb-3' >
         {drinks.map((drink) => (
-          <div key={drink.idDrink} className='grid grid-cols-2 gap-2 md:gap-4 border border-white/30 bg-white/60 rounded-lg'>
+          <div key={drink.idDrink} className='grid grid-cols-2 gap-3 md:gap-4 border border-white/30 bg-white/60 rounded-lg'>
             <div className='rounded-l-lg'>
               <img className='object-cover w-full h-full rounded-l-lg' src={drink.strDrinkThumb} alt={drink.strDrink} />
             </div>
