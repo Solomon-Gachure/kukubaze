a13099dce1a2f98f5dc943e4446b985285c0ea76
Solomon Gachure <gachuresolomon64@gmail.com>
2024-03-09 06:13:41 +0300

    Created maps section and added the map to it

--- src/component/map.jsx
+++ src/component/map.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Map = () => {
   return (
-    <div>Map</div>
+    <div className='pt-4 bg-stone-950'>
+      <div className='w-full'>
+        <iframe className='w-full h-[300px]'
+        src="https://www.google.com/maps/embed/v1/place?q=Kuku+Jamboo,+Murang'a,+Kenya&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8">
+          </iframe>
+          </div>
+            </div>
+    
   )
 }
 
