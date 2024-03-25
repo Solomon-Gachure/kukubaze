77da60d1ae87c71480df4bd52dea93100f79eb1b
Solomon Gachure <gachuresolomon64@gmail.com>
2024-03-25 15:09:24 +0300

    Added functionality to add and remove items from & to the cart

--- src/component/cartcontext.jsx
+++ src/component/cartcontext.jsx
@@ -1,8 +1,20 @@
 import React, { createContext, useState } from 'react'
 import {PRODUCTS} from '../data/products'
-export const cartContext= createContext(null)
+export const cartContext= createContext(null);
+const getDefaultCart=()=>{
+  let cart={};
+  for(let i=1; i<PRODUCTS.length +1; i++){
+    cart[i]=0;
+  }
+}
 const Cartcontext = (props) => {
   const [cartItems, setCartItems] = useState()
+  const addToCart=(itemId)=>{
+    setCartItems((prev)=>({...prev, [itemId]: prev[itemId] + 1}));
+  };
+  const removeFromCart=(itemId)=>{
+    setCartItems((prev)=>({...prev, [itemId]: prev[itemId] - 1}));
+  };
   return (
     <cartContext.Provider>
       {props.children}
