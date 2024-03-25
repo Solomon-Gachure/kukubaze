a2432a0ffbc88fa630e5a0f006b2b3ce0640704e
Solomon Gachure <gachuresolomon64@gmail.com>
2024-03-25 15:13:02 +0300

    passed values of the cart functionality through the context cart function

--- src/component/cartcontext.jsx
+++ src/component/cartcontext.jsx
@@ -1,25 +1,26 @@
-import React, { createContext, useState } from 'react'
-import {PRODUCTS} from '../data/products'
-export const cartContext= createContext(null);
-const getDefaultCart=()=>{
-  let cart={};
-  for(let i=1; i<PRODUCTS.length +1; i++){
-    cart[i]=0;
+import React, { createContext, useState } from "react";
+import { PRODUCTS } from "../data/products";
+export const cartContext = createContext(null);
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length + 1; i++) {
+    cart[i] = 0;
   }
-}
+};
 const Cartcontext = (props) => {
-  const [cartItems, setCartItems] = useState()
-  const addToCart=(itemId)=>{
-    setCartItems((prev)=>({...prev, [itemId]: prev[itemId] + 1}));
+  const [cartItems, setCartItems] = useState();
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
   };
-  const removeFromCart=(itemId)=>{
-    setCartItems((prev)=>({...prev, [itemId]: prev[itemId] - 1}));
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
+  const contextValue={cartItems, addToCart,removeFromCart}
   return (
-    <cartContext.Provider>
-      {props.children}
+  <cartContext.Provider value={contextValue}>
+    {props.children}
     </cartContext.Provider>
-  );
-}
+    );
+};
 
-export default Cartcontext
+export default Cartcontext;
