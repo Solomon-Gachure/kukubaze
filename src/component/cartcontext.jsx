59ac40d2aa45fdd750353fcafadf24585e1d4667
Solomon Gachure <gachuresolomon64@gmail.com>
2024-03-25 15:00:19 +0300

    Added the products to the cart context

--- src/component/cartcontext.jsx
+++ src/component/cartcontext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react'
-
+import {PRODUCTS} from '../data/products'
 export const cartContext= createContext(null)
 const Cartcontext = (props) => {
   const [cartItems, setCartItems] = useState()
